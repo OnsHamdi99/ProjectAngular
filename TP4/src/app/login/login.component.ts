b33740c9401be3b32a3e172c2d4f105f712a95c9
OnsHamdi99 <77619552+OnsHamdi99@users.noreply.github.com>
2023-01-11 18:23:49 +0100

    problem page login

--- TP4/src/app/login/login.component.ts
+++ TP4/src/app/login/login.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router} from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  password : string = ""; 
+  email : string="";
+  url = "http://localhost:8010/api/auth/AuthContoller";
+  constructor(private http: HttpClient, private router:Router,private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
+  login(email : string, password:string){
+    const body = { email: email, password: password };
+    this.http.post(this.url + '/login', body).subscribe(
+      response => { 
+        this.router.navigate(['/home']);
+      }, 
+      (error) => {
+      this.snackBar.open("Identifiants faux", "Dismiss", {duration: 2000}); } 
+    )
+  }
+  
+
+  
 
 }
