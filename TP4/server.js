f8dbea7b11e0ee72900500205fdc65d2ffa5d2c2
TeoFeruglio <90459174+TeoFeruglio@users.noreply.github.com>
2023-01-16 19:35:32 +0100

    Update : test for hosting 3

--- TP4/server.js
+++ TP4/server.js
@@ -1,15 +1,14 @@
 //Install express server
-const path = require("path");
 const express = require("express");
-
+const path = require("path");
 
 const app = express();
-    
+
 // Serve only the static files form the dist directory
-app.use(express.static(dirname + "/dist/assignment-app/"));
+app.use(express.static(__dirname + "/dist/assignment-app/"));
 
 app.get("/*", function (req, res) {
- res.sendFile(path.join(dirname + "/dist/assignment-app/index.html"));
+ res.sendFile(path.join(__dirname + "/dist/assignment-app/index.html"));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8081);
